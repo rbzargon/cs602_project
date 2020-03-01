5eb81f77c0d891604541c7b0fa1a15df83c2733a
Cooper King <cpking2@bu.edu>
2020-02-29 19:07:02 -0500

    Initial view

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { OrderModule } from './order/order.module';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -12,12 +14,16 @@ import { UserModule } from './user/user.module';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
       }),
-      inject: [ConfigService]
+      inject: [ConfigService],
     }),
     OrderModule,
     ProductModule,
     UserModule,
   ],
+  controllers: [AppController],
+  providers: [AppService]
 })
 export class AppModule { }
