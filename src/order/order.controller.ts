397abfb6488d0cb83f5ed20f1cf84c2fbfd9f61a
Cooper King <cpking2@bu.edu>
2020-03-01 07:00:40 -0500

    Fix order controller

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Header } from '@nestjs/common';
+import { Controller, Header, Get, Render } from '@nestjs/common';
+import { Order } from './model/order.interface';
+import { OrderService } from './order.service';
 
 @Controller('order')
 export class OrderController {
 
+    constructor(private readonly orderService: OrderService) { }
+
     @Get()
     @Header('Content-Type', 'text-html')
     @Render('order/index')
-    async getAll: Promise<{orders: Order[]}> {
+    async getAll(): Promise<{orders: Order[]}> {
         const orders = await this.orderService.findAll();
         return { orders };
     }
