a1b1ccaa6a67dd0cf8b7ef2cbe140cdd9db06b79
Cooper King <cpking2@bu.edu>
2020-03-05 20:32:54 -0500

    Add parseInt to port

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   }));
 
   // app.enableCors()
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(parseInt(process.env.PORT, 10) || 3000);
 }
 bootstrap();
